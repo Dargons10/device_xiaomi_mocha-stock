03bda0c2e375ea647fb2f218f7e85d57bdfd2795
dargons10 <jdamr2p@gmail.com>
2024-08-19 19:16:00 +0200

    update shims

--- shims/Android.mk
+++ shims/Android.mk
@@ -26,7 +26,19 @@ LOCAL_MODULE := libs
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
+## libshim_zw
+include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES := zygote_whitelist.cpp
+LOCAL_C_INCLUDES := frameworks/base/core/jni \
+                    system/core/base/include
+ifneq ($(ZYGOTE_WHITELIST_PATH_EXTRA),)
+    LOCAL_CFLAGS += -DPATH_WHITELIST_EXTRA=$(ZYGOTE_WHITELIST_PATH_EXTRA)
+endif
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE := libshim_zw
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
 
 ## EGL Shim
 include $(CLEAR_VARS)
