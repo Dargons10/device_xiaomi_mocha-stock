f0707e93b4aa5d5faf3c569e9614256d321aa027
dargons10 <jdamr2p@gmail.com>
2024-08-28 17:21:12 +0200

    mocha: Enable boot and system server dex-preopt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,6 +51,16 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 #FM
 BOARD_HAVE_BCM_FM := true
 
