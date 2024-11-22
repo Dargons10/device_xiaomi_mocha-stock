074a16832aa82d7c2d2da431998189f0017f5c0c
dargons10 <jdamr2p@gmail.com>
2024-11-22 19:34:16 +0100

    mocha: Add fingerprint

--- lineage.mk
+++ lineage.mk
@@ -11,4 +11,6 @@ PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/lineage_mocha/mocha:8.1.0/OPM7.181205.001/fa32f5c546:eng/test-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
