18dd9fa89adaab9c9910cf8c2ca61869706c1a2f
dargons10 <jdamr2p@gmail.com>
2024-11-25 22:39:25 +0100

    mocha: libshims: Add zygote whitelist

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,3 +158,6 @@ WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
 #WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
 #WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+
+# Zygote whitelist extra paths
+ZYGOTE_WHITELIST_PATH_EXTRA := \"/dev/nvhost-ctrl\",\"/dev/nvmap\",
