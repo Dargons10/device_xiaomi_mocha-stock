c7ad596e61d85c0f09adbc43d7492bc2495f4e02
dargons10 <jdamr2p@gmail.com>
2024-11-25 22:40:16 +0100

    mocha: libshims: add vnsprintf for old blobs

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -54,3 +54,24 @@ LOCAL_SRC_FILES := libshim_utils.cpp
 LOCAL_SHARED_LIBRARIES := libutils
 LOCAL_MODULE := libshim_utils
 include $(BUILD_SHARED_LIBRARY)
+
+## liblog
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := stdio_vsnprintf.cpp
+LOCAL_C_INCLUDES := bionic/libc/stdio
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE := libs
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := zygote_whitelist.cpp
+LOCAL_C_INCLUDES := frameworks/base/core/jni \
+                    system/core/base/include
+ifneq ($(ZYGOTE_WHITELIST_PATH_EXTRA),)
+    LOCAL_CFLAGS += -DPATH_WHITELIST_EXTRA=$(ZYGOTE_WHITELIST_PATH_EXTRA)
+endif
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_MODULE := libshim_zw
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
