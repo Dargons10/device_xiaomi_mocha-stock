9714a81bb04d736595f37e6df89f1b400fb1e797
dargons10 <jdamr2p@gmail.com>
2025-01-05 20:35:07 +0100

    mocha: Fix path to apply Bionic patches

--- patches/install.sh
+++ patches/install.sh
@@ -4,7 +4,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic/libs frameworks/base frameworks/native"
+dirs="bionic/libm frameworks/base frameworks/native"
 
 # red + nocolor
 RED='\033[0;31m'
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="bionic/libs frameworks/base frameworks/native"
+dirs="bionic/libm frameworks/base frameworks/native"
 
 for dir in $dirs ; do
 	cd $rootdirectory
