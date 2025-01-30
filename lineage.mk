818508391f422a5af14dbba3397ecbd0094dc189
dargons10 <jdamr2p@gmail.com>
2025-01-30 20:18:14 +0100

    mocha: Fix build and fingerprint

--- lineage.mk
+++ lineage.mk
@@ -11,7 +11,10 @@ PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/lineage_mocha/mocha:8.1.0/OPM7.181205.001/fa32f5c546:eng/test-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="mocha-userdebug 8.1.0 OPM7.181205.001 V9.2.4.0.KXFCNEK release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/mocha/mocha:8.1.0/OPM7.181205.001/V9.2.4.0.KXFCNEK:userdebug/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
