f4525a1bd672f50059b8d00ab3b9738deed61a6a
dargons10 <jdamr2p@gmail.com>
2025-01-30 20:16:09 +0100

    mocha: Add blob, tos, misc and firmware

--- lineage.mk
+++ lineage.mk
@@ -14,3 +14,6 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/lineage_mocha/mocha:8.1.0/OPM7.181205.001/fa32f5c546:eng/test-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Include firmware
+$(call inherit-product, vendor/xiaomi-firmware/mocha/firmware.mk)
