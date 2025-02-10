b714e111c4aa6be1aa52b5bd3a30e54c5f27a420
dargons10 <jdamr2p@gmail.com>
2025-02-10 21:19:12 +0100

    mocha: syste_prop: override lmkd options

--- system_prop.mk
+++ system_prop.mk
@@ -16,6 +16,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.lineage.nofool=true
 
+# LMKD options
+PRODUCT_PROPERTY_OVERRIDES += \
+ 	ro.lmk.low=1001 \
+ 	ro.lmk.medium=800 \
+ 	ro.lmk.critical=0 \
+ 	ro.lmk.critical_upgrade=false \
+ 	ro.lmk.upgrade_pressure=100 \
+ 	ro.lmk.downgrade_pressure=100 \
+ 	ro.lmk.kill_heaviest_task=true \
+ 	ro.lmk.kill_timeout_ms=100
+
 # Nvmm
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.tegra.nvmmlite = 1
