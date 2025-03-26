3a3296742c9e591e18e257fd7c413a688b8dc31d
dargons10 <jdamr2p@gmail.com>
2025-03-26 21:27:33 +0100

    mocha: device: Added bcm4354 firmware

--- mocha.mk
+++ mocha.mk
@@ -291,6 +291,7 @@ PRODUCT_COPY_FILES += \
 # Wifi
 # All Shield devices xurrently use broadcom wifi / bluetooth modules
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
