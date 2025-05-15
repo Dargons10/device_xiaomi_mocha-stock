b4b2bf6785db07d142e2959413ea13af129bb4d6
Dargons10 <jdamr2p@gmail.com>
2025-05-15 14:14:23 +0200

    mocha: device: add languages_full

--- mocha.mk
+++ mocha.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
 LOCAL_PATH := device/xiaomi/mocha
