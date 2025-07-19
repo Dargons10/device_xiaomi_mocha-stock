07b16fcdeed92cbc9e1b92c729f4ddd22be677c2
Dargons10 <jdamr2p@gmail.com>
2025-07-19 10:30:13 +0200

    mocha: RadioFM: Partially fixed

--- mocha.mk
+++ mocha.mk
@@ -114,9 +114,11 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
+    android.hardware.broadcastradio@1.0-impl \
     FMRadio \
     brcm-uim-sysfs \
-    libfmjni
+    libfmjni \
+    libfmradio.v4l2-fm
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
