0543ba9ee03919212541b00c683f00ee948ffdfb
Dargons10 <jdamr2p@gmail.com>
2025-07-30 23:14:53 +0200

    mocha: audio: upgrade to AudioHAL 4.0 for Pie

--- mocha.mk
+++ mocha.mk
@@ -29,10 +29,9 @@ TARGET_TEGRA_VERSION := t124
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0 \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
