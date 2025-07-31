e9a5dfc2bedee35f681ab517f4dfc077434fbee0
Dargons10 <jdamr2p@gmail.com>
2025-07-31 20:10:42 +0200

    mocha: Health: Updated to 2.0

--- mocha.mk
+++ mocha.mk
@@ -135,8 +135,8 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL (needed for shield 8.0 based hidl hals)
 PRODUCT_PACKAGES += \
