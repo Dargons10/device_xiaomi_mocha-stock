e8118187cb8fc69c0fdeb6303cfa8ce080a86233
Dargons10 <jdamr2p@gmail.com>
2026-04-03 13:57:46 +0200

    mocha: drop firmware.mk path from lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -20,5 +20,3 @@ BUILD_FINGERPRINT := Xiaomi/mocha/mocha:8.1.0/OPM7.181205.001/V9.2.4.0.KXFCNEK:u
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Include firmware
-$(call inherit-product, vendor/xiaomi-firmware/mocha/firmware.mk)
